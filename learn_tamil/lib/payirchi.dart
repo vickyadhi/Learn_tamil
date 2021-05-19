4cbd8f7732eb5b0032f67a441fd506b1fde3ba8c
vickyadhi <vickyadhi011@gmail.com>
2021-05-19 10:12:16 -0700

    Segmentation button added

--- learn_tamil/lib/payirchi.dart
+++ learn_tamil/lib/payirchi.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:pie_chart/pie_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:material_segmented_control/material_segmented_control.dart';
 import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
 
 class Payirchi extends StatefulWidget {
@@ -27,13 +29,27 @@ class _PayirchiState extends State<Payirchi> with SingleTickerProviderStateMixin
      animationController.repeat();
     super.initState();
   }
+  int _currentSelection=0;
+  int _BoxShape;
+
 
 
   @override
   Widget build(BuildContext context) {
+    final Map<String,double> dataMap={
+      "Katral":4,
+      "Thervu":9,
+    };
+    PieChart(dataMap: dataMap);
+    final Map<int,Widget>_children=const<int,Widget>{
+    0:Text('முகப்பு',style: TextStyle(color: Colors.black),),
+      1:Text('எண்களை அறிந்து கொள்ள',style: TextStyle(color: Colors.black),)
+    };
     final percentage = animationController.value*100;
+    var colorList;
     return MaterialApp(
       home:Scaffold(
+
         appBar: AppBar(
           backgroundColor: Colors.cyan[600],
           title: Text(
@@ -50,6 +66,19 @@ class _PayirchiState extends State<Payirchi> with SingleTickerProviderStateMixin
           toolbarHeight: 180,
           elevation: 20,
         ),
+        floatingActionButton: Container(
+          width: MediaQuery.of(context).size.width*2.89,
+
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(50, 110, 10, 640),
+            child: FloatingActionButton.extended(
+              elevation: 19,
+              label: const Text("பயிற்சி",style: TextStyle(color: Colors.black87),),
+              icon: const Icon(Icons.work,size: 25.0,color: Colors.white,),
+              backgroundColor: Colors.cyan
+            ),
+          ),
+        ),
         body: Container(
           decoration: BoxDecoration(
               gradient: LinearGradient(
@@ -64,9 +93,30 @@ class _PayirchiState extends State<Payirchi> with SingleTickerProviderStateMixin
             crossAxisAlignment: CrossAxisAlignment.center,
            // mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              Padding(padding: EdgeInsets.symmetric(vertical: 80)),
+             Padding(
+               padding: const EdgeInsets.symmetric(vertical: 90),
+               child: Container(
+                 width: MediaQuery.of(context).size.width*1.2,
+                 child: MaterialSegmentedControl(
+                       children: _children,
+                       selectionIndex: _currentSelection,
+                       borderColor: Colors.black12,
+                       selectedColor: Colors.cyan[200],
+                       unselectedColor: Colors.white,
+                       borderRadius: 32.0,
+                       disabledChildren: [
+                         2,
+                       ],
+                       onSegmentChosen: (index) {
+      setState(() {
+      _currentSelection = index;
+      });
+      },),
+               ),
+             ),
+              Padding(padding: EdgeInsets.symmetric(vertical: 0,horizontal: 80)),
               Container(
-                padding: EdgeInsets.fromLTRB(40.0, 1, 40.0, 60),
+                padding: EdgeInsets.fromLTRB(40.0, 1, 30.0, 70),
                 width: double.infinity,
                 height: 120,
                 child: LiquidLinearProgressIndicator(
@@ -84,29 +134,27 @@ class _PayirchiState extends State<Payirchi> with SingleTickerProviderStateMixin
                 ),
               ),
               Container(
-                padding: EdgeInsets.fromLTRB(40.0, 1, 40.0, 60),
-                width: double.infinity,
-                height: 120,
-                child: LiquidLinearProgressIndicator(
-                  value: 0.4,
-                  valueColor: AlwaysStoppedAnimation(Colors.white60),
-                  center: Text('40',
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black87
-                  ),
-                  ),
-                  direction: Axis.horizontal,
-                  backgroundColor: Colors.cyan.shade300,
-
-
-                )
-              )
+                  padding: EdgeInsets.fromLTRB(40.0, 1, 30.0, 70),
+                  width: double.infinity,
+                  height: 120,
+                  child: LiquidLinearProgressIndicator(
+                    value: 0.4,
+                    valueColor: AlwaysStoppedAnimation(Colors.white60),
+                    center: Text('40',
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black87
+                      ),
+                    ),
+                    direction: Axis.horizontal,
+                    backgroundColor: Colors.cyan.shade300,
+                  )
+              ),
             ],
           ),
         ),
-      )
+      ),
     );
   }
 }
